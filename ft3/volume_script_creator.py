64c485940297fe7328186aff9660dd5895821af2
Martin <martinperezmaneiro@gmail.com>
2023-06-09 16:38:59 +0200

    Minor changes

--- ft3/volume_script_creator.py
+++ ft3/volume_script_creator.py
@@ -19,9 +19,10 @@ for v in vols:
     #for c in ['sophronia', 'esmeralda', 'beersheba', 'isaura']:
     #    os.system('mv $LUSTRE/NEXT100/data/full_prod/214Bi/prod/volumes/{vol}/prod/{city}/ $LUSTRE/NEXT100/data/full_prod/214Bi/prod/volumes/{vol}/prod/ref_prod/'.format(vol = v, city = c))
     #os.system('rm $LUSTRE/NEXT100/data/full_prod/214Bi/prod/volumes/{vol}/logs/*'.format(vol = v))
+    os.system('python nexus_compressor.py &')
     #os.system('python create_task_files.py')
     #os.system('python create_job_files.py')
-    os.system('python job_launcher.py')
+    #os.system('python job_launcher.py')
 
 os.system("sed -i 's\/{prev}/\/{post}/\g' config.py".format(prev = prev, post = 'volumes'))
 
